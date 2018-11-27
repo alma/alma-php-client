a15d1870c0bb55f7bd15d374226d40bcc17600d1
Olivier Lance <olivier.lance@getalma.eu>
2018-11-27 17:40:57 +0100

    Adds PSR-4 autoloader

---
+++ autoload.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * From https://www.php-fig.org/psr/psr-4/examples/.
+ */
+spl_autoload_register(function ($class) {
+    // project-specific namespace prefix
+    $prefix = 'Alma';
+    // base directory for the namespace prefix
+    $base_dir = __DIR__ . '/src/';
+    // does the class use the namespace prefix?
+    $len = strlen($prefix);
+    if (0 !== strncmp($prefix, $class, $len)) {
+        // no, move to the next registered autoloader
+        return;
+    }
+    // get the relative class name
+    $relative_class = substr($class, $len);
+
+    // replace the namespace prefix with the base directory, replace namespace
+    // separators with directory separators in the relative class name, append
+    // with .php
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+    // if the file exists, require it
+    if (file_exists($file)) {
+        require $file;
+    }
+});
