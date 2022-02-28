40eb4b7c117c838cbd6442ad5b7b4d7c711c6ae3
Paul Roussel <paul.roussel-ext@getalma.eu>
2022-02-28 11:00:47 +0100

    sorted tests in proper folder + added faulty test for config

--- tests/ArrayUtilsTest.php
+++ tests/ArrayUtilsTest.php
@@ -3,90 +3,13 @@
 namespace Alma\API\Tests;
 
 use Alma\API\Lib\ArrayUtils;
-use Alma\API\Lib\ClientOptionsValidator;
-use Alma\API\Client;
 use PHPUnit\Framework\TestCase;
-use Psr\Log\NullLogger;
-use const Alma\API\LIVE_MODE;
-use const Alma\API\TEST_MODE;
 
 /**
  * Class ArrayUtils
  */
 class ArrayUtilsTest extends TestCase
 {
-    /** @var string  */
-    const FAKE_API_URI = 'https://fake-api.getalma.eu';
-
-    /**
-     * Return options to test ArrayUtils::almaArrayMergeRecursive
-     * @return array
-     */
-    public function getClientOptions()
-    {
-        return [
-            [
-                [],
-                [
-                    'api_root' => [
-                        Client::TEST_MODE => Client::SANDBOX_API_URL,
-                        Client::LIVE_MODE => Client::LIVE_API_URL
-                    ],
-                    'force_tls' => 2,
-                    'mode' => Client::LIVE_MODE,
-                    'logger' => new NullLogger()
-                ]
-            ],
-            [
-                [
-                    'api_root' => [
-                        Client::TEST_MODE => self::FAKE_API_URI,
-                        Client::LIVE_MODE => self::FAKE_API_URI
-                    ],
-                    'force_tls' => 0,
-                    'mode' => Client::TEST_MODE,
-                    'logger' => new NullLogger()
-                ],
-                [
-                    'api_root' => [
-                        Client::TEST_MODE => self::FAKE_API_URI,
-                        Client::LIVE_MODE => self::FAKE_API_URI
-                    ],
-                    'force_tls' => 0,
-                    'mode' => Client::TEST_MODE,
-                    'logger' => new NullLogger()
-                ]
-            ],
-            [
-                [
-                    'api_root' => self::FAKE_API_URI,
-                    'force_tls' => true,
-                    'logger' => new NullLogger()
-                ],
-                [
-                    'api_root' => [
-                        Client::TEST_MODE => self::FAKE_API_URI,
-                        Client::LIVE_MODE => self::FAKE_API_URI
-                    ],
-                    'force_tls' => 2,
-                    'mode' => Client::LIVE_MODE,
-                    'logger' => new NullLogger()
-                ]
-            ]
-        ];
-    }
-
-    /**
-     * @dataProvider getClientOptions
-     * @return void
-     */
-    public function testClientOptionsValidator($options, $expectedResult)
-    {
-        $validatedConfig = ClientOptionsValidator::validateOptions($options);
-
-        $this->assertEquals($expectedResult, $validatedConfig);
-    }
-
     /**
      * Return options to test ArrayUtils::isAssocArray
      * @return array[]
