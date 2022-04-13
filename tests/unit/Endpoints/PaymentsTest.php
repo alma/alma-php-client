7110d40dc93e645c7fe364fe2da8e4fdcab1f897
Paul Roussel <paul.roussel-ext@getalma.eu>
2022-04-13 10:06:33 +0200

    refacto: unit test refactored

--- tests/unit/Endpoints/PaymentsTest.php
+++ tests/unit/Endpoints/PaymentsTest.php
@@ -24,12 +24,7 @@ class PaymentsTest extends TestCase
         $this->assertEquals(true, method_exists($payments, 'fullRefund'));
     }
 
-    public function testPartialRefund()
-    {
-        /* Input */
-        $id = 1;
-
-        /* Mock */
+    private function mockServerRequest() {
         // ClientContext
         $clientContext = Mockery::mock(ClientContext::class);
         $clientContext->shouldReceive('urlFor');
@@ -46,6 +41,13 @@ class PaymentsTest extends TestCase
         $requestMock = Mockery::mock(Request::class);
         $requestMock->shouldReceive('setRequestBody')->once();
         $requestMock->shouldReceive('post')->andReturn($responseMock);
+        return $requestMock;
+    }
+
+    public function testPartialRefund()
+    {
+        /* Input */
+        $id = 1;
 
         // Payment
         $payments = Mockery::mock(Payments::class)
@@ -55,7 +57,7 @@ class PaymentsTest extends TestCase
         $payments->shouldReceive('request')
             ->with("/v1/payments/$id/refund")
             ->once()
-            ->andReturn($requestMock)
+            ->andReturn($this->mockServerRequest())
         ;
         $payments->setClientContext($clientContext);
 
@@ -66,25 +68,7 @@ class PaymentsTest extends TestCase
     public function testFullRefund()
     {
         /* Input */
-        $id = 1;
-
-        /* Mock */
-        // ClientContext
-        $clientContext = Mockery::mock(ClientContext::class);
-        $clientContext->shouldReceive('urlFor');
-        $clientContext->shouldReceive('getUserAgentString');
-        $clientContext->shouldReceive('forcedTLSVersion');
-
-        // Response
-        $json = '{"payment_plan":[{"customer_can_postpone":false,"customer_fee":100,"customer_interest":0,"date_paid":1649672472,"due_date":1649672451,"id":"installment_11uPRjPgPJ5jxmEEXxIPPOvVA3Fh7cg13m","original_purchase_amount":11733,"purchase_amount":11733,"state":"paid"},{"customer_can_postpone":false,"customer_fee":0,"customer_interest":0,"date_paid":null,"due_date":1652264451,"id":"installment_11uPRjP4a3cFQvoM1lehDnpxa2tYm2Hy0I","original_purchase_amount":11733,"purchase_amount":0,"state":"paid"},{"customer_can_postpone":false,"customer_fee":0,"customer_interest":0,"date_paid":null,"due_date":1654942851,"id":"installment_11uPRjP79fG5QtHO54lSYsnDvyHb56oeCn","original_purchase_amount":11733,"purchase_amount":0,"state":"paid"}],"orders":[{"comment":null,"created":1649672451,"customer_url":null,"data":{},"id":"order_11uPRjP4L9Dgbttx3cFUKGFPppdZIlrR2V","merchant_reference":"00000206","merchant_url":null,"payment":"payment_11uPRjP5FaIYygQao8WdMQFnKRMOV14frx"}],"refunds":[{"amount":35299,"created":1649770695,"from_payment_cancellation":false,"id":"refund_11uPrHz4TfHmQrD1OkYyWb4hPuq3673Vqm","merchant_reference":null,"payment":"payment_11uPRjP5FaIYygQao8WdMQFnKRMOV14frx","payment_refund_amount":11833,"rebate_amount":23466}]}';
-        $responseMock = Mockery::mock(Response::class);
-        $responseMock->shouldReceive('isError')->andReturn(false);
-        $responseMock->json = json_decode($json, true);
-
-        // Request
-        $requestMock = Mockery::mock(Request::class);
-        $requestMock->shouldReceive('setRequestBody')->once();
-        $requestMock->shouldReceive('post')->andReturn($responseMock);
+        $id = 2;
 
         // Payment
         $payments = Mockery::mock(Payments::class)
@@ -94,7 +78,7 @@ class PaymentsTest extends TestCase
         $payments->shouldReceive('request')
             ->with("/v1/payments/$id/refund")
             ->once()
-            ->andReturn($requestMock)
+            ->andReturn($this->mockServerRequest())
         ;
         $payments->setClientContext($clientContext);
 
