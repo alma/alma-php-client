c25a44c320fdb4444b34960cc1541194c8fae96d
Paul Roussel <paul.roussel-ext@getalma.eu>
2022-07-01 10:34:10 +0200

    feat: Added getErrorMessage method to RequestError

--- src/RequestError.php
+++ src/RequestError.php
@@ -22,4 +22,23 @@ class RequestError extends Exception
         $this->request = $request;
         $this->response = $response;
     }
+
+    public function getErrorMessage()
+    {
+        $message = $this->getMessage();
+
+        if ($message) {
+            return $message;
+        }
+
+        if (isset($this->response->json)
+            && isset($this->response->json['errors'])
+            && isset($this->response->json['errors'][0])
+            && isset($this->response->json['errors'][0]['message'])
+            ) {
+            return $this->response->json['errors'][0]['message'];
+        }
+
+        return '';
+    }
 }
