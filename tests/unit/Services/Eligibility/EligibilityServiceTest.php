77ad3d8dbd9b0aa4b73f48f94d37fa746075d926
Sylvain Just <sylvain.just-ext@getalma.eu>
2022-09-16 17:09:22 +0200

    wip: EligibilityServiceTest impl in progess

---
+++ tests/unit/Services/Eligibility/EligibilityServiceTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Alma\API\Tests\unit\Services\Eligibility;
+
+use Alma\API\Services\Eligibility\EligibilityService;
+use PHPUnit\Framework\TestCase;
+
+class EligibilityServiceTest extends TestCase
+{
+
+    public function testGetList()
+    {
+
+    }
+}
