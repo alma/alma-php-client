96b3281120efaa3b0d7f49fd8c887c22a5eb38f5
Benjamin Freoua <benjamin.freoua@getalma.eu>
2024-02-16 10:48:01 +0100

    fix sonar

--- tests/Unit/Legacy/Endpoints/InsuranceTest.php
+++ tests/Unit/Legacy/Endpoints/InsuranceTest.php
@@ -750,7 +750,7 @@ class InsuranceTest extends TestCase
      */
     public function testGetSubscriptionRequestIsCalled($subscriptionIds, $json)
     {
-        $this->responseMock->json = json_decode($json,true);
+        $this->responseMock->json = json_decode($json, true);
         $this->responseMock->shouldReceive('isError')->once()->andReturn(false);
         $this->requestObject->shouldReceive('get')->once()->andReturn($this->responseMock);
         $this->requestObject->shouldReceive('setQueryParams')
@@ -821,7 +821,7 @@ class InsuranceTest extends TestCase
      */
     public function testGetSubscriptionsReturnApiResponse($subscriptionIds, $json)
     {
-        $this->responseMock->json = json_decode($json,true);
+        $this->responseMock->json = json_decode($json, true);
         $this->responseMock->shouldReceive('isError')->once()->andReturn(false);
         $this->requestObject->shouldReceive('get')->once()->andReturn($this->responseMock);
         $this->requestObject->shouldReceive('setQueryParams')->once()->andReturn($this->requestObject);
@@ -955,7 +955,7 @@ class InsuranceTest extends TestCase
      */
     public function testGetSubscriptionIfReturnZeroDataThrowException($subscriptionIdInvalid, $jsonReturnRequest)
     {
-        $this->responseMock->json = json_decode($jsonReturnRequest,true);
+        $this->responseMock->json = json_decode($jsonReturnRequest, true);
         $this->responseMock->shouldReceive('isError')->once()->andReturn(false);
         $this->requestObject->shouldReceive('setQueryParams')->once()->andReturn($this->requestObject);
         $this->requestObject->shouldReceive('get')->once()->andReturn($this->responseMock);
