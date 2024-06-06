80a0ec61c9656cb6b77da08e215d868e2702bab8
Guillaume Desvé <guillaume.desve@almapay.com>
2024-06-06 11:22:38 +0200

    Update the local docker development experience (#107)

--- build.sh
+++ build.sh
@@ -1,18 +1,6 @@
 #!/usr/bin/env bash
 
-DIR=`pwd`
-
-rm -rf ./dist/
-rm -rf /tmp/alma-php-client
-mkdir -p /tmp/alma-php-client
-
-cp -r ./* /tmp/alma-php-client/
-
-mkdir ./dist
-
-cd /tmp/alma-php-client/ || exit
-rm -rf vendor
+rm -rf dist/ vendor/
+mkdir -p ./dist
 composer install --no-dev
-zip -9 -r "$DIR/dist/alma-php-client.zip" . --exclude \*dist\* \*.git\* \*.idea\* \*.DS_Store
-
-rm -rf /tmp/alma-php-client
+zip -9 -r "dist/alma-php-client.zip" src/ README.md CHANGELOG.md LICENSE composer.json
