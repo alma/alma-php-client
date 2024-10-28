c9d53b5e9268eb8b86c7d944f61dc9b5d147fcb7
joyet simon <43644110+joyet-simon@users.noreply.github.com>
2024-10-28 15:57:55 +0100

    feat: make isUrlRefreshRequired static

--- src/Lib/IntegrationsConfigurationsUtils.php
+++ src/Lib/IntegrationsConfigurationsUtils.php
@@ -8,7 +8,7 @@ class IntegrationsConfigurationsUtils
      * @param int | null $lastSendTimestamp
      * @return bool
      */
-    public function isUrlRefreshRequired($lastSendTimestamp)
+    public static function isUrlRefreshRequired($lastSendTimestamp)
     {
         $oneMonthInSeconds = 30 * 24 * 60 * 60; // 30 jours en sec
         return (time() - $lastSendTimestamp) > $oneMonthInSeconds;
