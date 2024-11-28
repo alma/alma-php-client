2396eabc20225b0a6a3e2efb386999bd6cb8f396
Carine Bonnafous <carine.bonnafous@getalma.eu>
2024-11-28 17:42:28 +0100

    Patch reportportal/basic to make it compliant with ReportPortal api/v2

--- tests/reportportal_conf.sh
+++ tests/reportportal_conf.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# exit if REPORT_PORTAL_API_KEY or REPORT_PORTAL_ENDPOINT is not set
+if [ -z "$REPORT_PORTAL_API_KEY" ] || [ -z "$REPORT_PORTAL_ENDPOINT" ]; then
+    echo "Please set REPORT_PORTAL_API_KEY and REPORT_PORTAL_ENDPOINT environment variables"
+    exit 1
+fi
+
 # Remove /api/v1 from REPORT_PORTAL_ENDPOINT (reportportal/agent-php-PHPUnit requires only host)
 export REPORT_PORTAL_HOST=${REPORT_PORTAL_ENDPOINT/\/api\/v1/}
 
@@ -19,3 +25,9 @@ sed -i $'/<\/phpunit>/{e cat tests/reportportal_phpunit_conf.xml\n}' phpunit.ci.
 composer config minimum-stability dev
 # This is not supported by all versions of PHP, so we need to install it separately
 composer require --dev reportportal/phpunit
+
+# Patch reportportal/basic to make it compatible with api/v2
+# Use api/v2 instead of api/v1 (hardcoded in reportportal/basic)
+sed -i 's/v1\//v2\//g' vendor/reportportal/basic/src/service/ReportPortalHTTPService.php
+# Add launchUuid to finishItem method in reportportal/basic to make it compatible with api/v2
+sed -i "/function finishItem/,/}/s/\('status' => \$status\)/\1,\n'launchUuid' => self::\$launchID/" vendor/reportportal/basic/src/service/ReportPortalHTTPService.php
