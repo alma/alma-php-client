0c686f281383766ea3004c272f7a13a519a0a67b
Carine Bonnafous <carine.bonnafous@getalma.eu>
2024-11-29 10:56:48 +0100

    Use patch rather than sed to patch reportportal/basic

--- tests/reportportal/reportportal_conf.sh
+++ tests/reportportal/reportportal_conf.sh
@@ -6,19 +6,19 @@ if [ -z "$REPORT_PORTAL_API_KEY" ] || [ -z "$REPORT_PORTAL_ENDPOINT" ]; then
     exit 1
 fi
 
-# Remove /api/v1 from REPORT_PORTAL_ENDPOINT (reportportal/agent-php-PHPUnit requires only host)
-export REPORT_PORTAL_HOST=${REPORT_PORTAL_ENDPOINT/\/api\/v1/}
+# Remove /api/v1 or api/v2 from REPORT_PORTAL_ENDPOINT (reportportal/agent-php-PHPUnit requires only host)
+export REPORT_PORTAL_HOST=${REPORT_PORTAL_ENDPOINT/\/api\/v[12]/}
 
 # Add secrets values in tests/reportportal_phpunit_conf_template.xml
-# Following environment variables are required:
+# Following environment variables are used:
 # REPORT_PORTAL_API_KEY
 # REPORT_PORTAL_HOST
 # PHP_VERSION
-envsubst < tests/reportportal_phpunit_conf_template.xml > tests/reportportal_phpunit_conf.xml
+envsubst < tests/reportportal/reportportal_phpunit_conf_template.xml > tests/reportportal/reportportal_phpunit_conf.xml
 
 # Add conf for ReportPortal extension in phpunit.ci.xml
 # Inserts content of file tests/reportportal_phpunit_conf.xml before </phpunit> end tag in phpunit.ci.xml
-sed -i $'/<\/phpunit>/{e cat tests/reportportal_phpunit_conf.xml\n}' phpunit.ci.xml
+sed -i $'/<\/phpunit>/{e cat tests/reportportal/reportportal_phpunit_conf.xml\n}' phpunit.ci.xml
 
 # Add ReportPortal extension to composer.json
 # reportportal/phpunit has no stable version, so we set minimum stability to dev only when running tests
@@ -27,7 +27,7 @@ composer config minimum-stability dev
 composer require --dev reportportal/phpunit
 
 # Patch reportportal/basic to make it compatible with api/v2
-# Use api/v2 instead of api/v1 (hardcoded in reportportal/basic)
-sed -i 's/v1\//v2\//g' vendor/reportportal/basic/src/service/ReportPortalHTTPService.php
-# Add launchUuid to finishItem method in reportportal/basic to make it compatible with api/v2
-sed -i "/function finishItem/,/}/s/\('status' => \$status\)/\1,\n'launchUuid' => self::\$launchID/" vendor/reportportal/basic/src/service/ReportPortalHTTPService.php
+# Patch content:
+#   * Replace api/v1 (hardcoded in reportportal/basic) by api/v2
+#   * Add launchUuid to finishItem method (otherwise all tests will be marked as "interrupted")
+patch vendor/reportportal/basic/src/service/ReportPortalHTTPService.php tests/reportportal/ReportPortalHTTPService.php.patch
