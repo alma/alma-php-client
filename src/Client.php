9b7740456b1e5eccac109347aa0428195b916ef7
Francois-Gomis <97046219+Francois-Gomis@users.noreply.github.com>
2025-01-30 13:12:33 +0000

    chore: update version

--- src/Client.php
+++ src/Client.php
@@ -30,7 +30,7 @@ use Alma\API\Lib\ClientOptionsValidator;
 
 class Client
 {
-    const VERSION = '2.4.0';
+    const VERSION = '2.5.0';
 
     const LIVE_MODE = 'live';
     const TEST_MODE = 'test';
