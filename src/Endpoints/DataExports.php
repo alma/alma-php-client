1be5f7c9eb7e7f8a8cd39cabeee888e110a5278b
joyet simon <43644110+joyet-simon@users.noreply.github.com>
2025-02-26 15:32:35 +0100

    feat: add doc block and unit tests for DataExports endpoint

--- src/Endpoints/DataExports.php
+++ src/Endpoints/DataExports.php
@@ -26,42 +26,29 @@
 namespace Alma\API\Endpoints;
 
 use Alma\API\Entities\DataExport;
+use Alma\API\Exceptions\ParametersException;
+use Alma\API\Exceptions\RequestException;
 use Alma\API\ParamsError;
 use Alma\API\RequestError;
 
 class DataExports extends Base
 {
     const DATA_EXPORTS_PATH = '/v1/data-exports';
+    const ACCEPTED_FORMAT = ['csv', 'xlsx'];
 
     /**
      * @param $data
+     *
      * @return DataExport
-     * @throws ParamsError
-     * @throws RequestError
+     *
+     * @throws RequestException|RequestError
      */
     public function create($data)
     {
         $res = $this->request(self::DATA_EXPORTS_PATH)->setRequestBody($data)->post();
 
         if ($res->isError()) {
-            throw new RequestError($res->errorMessage, null, $res);
-        }
-
-        return new DataExport($res->json);
-    }
-
-    /**
-     * @param $data
-     * @return DataExport
-     * @throws ParamsError
-     * @throws RequestError
-     */
-    public function fetch($reportId)
-    {
-        $res = $this->request(self::DATA_EXPORTS_PATH . '/' . $reportId)->get();
-
-        if ($res->isError()) {
-            throw new RequestError($res->errorMessage, null, $res);
+            throw new RequestException($res->errorMessage, null, $res);
         }
 
         return new DataExport($res->json);
@@ -69,19 +56,43 @@ class DataExports extends Base
 
     /**
      * @param $reportId
-     * @param $format
+     *
+     * @return DataExport
+     *
+     * @throws RequestException|RequestError
+     */
+    public function fetch($reportId)
+    {
+        $res = $this->request(self::DATA_EXPORTS_PATH . '/' . $reportId)->get();
+
+        if ($res->isError()) {
+            throw new RequestException($res->errorMessage, null, $res);
+        }
+
+        return new DataExport($res->json);
+    }
+
+    /**
+     * @param $reportId
+     *
+     * @param string $format only csv or xlsx
      *
      * @return mixed
-     * @throws RequestError
+     *
+     * @throws RequestException|RequestError|ParametersException
      */
     public function download($reportId, $format)
     {
+        if (!in_array($format, self::ACCEPTED_FORMAT)) {
+            throw new ParametersException("Invalid format: $format. Accepted format are: " . implode(', ', self::ACCEPTED_FORMAT));
+        }
+
         $res = $this->request(self::DATA_EXPORTS_PATH . '/' . $reportId)
             ->setQueryParams(['format' => $format])
             ->get();
 
         if ($res->isError()) {
-            throw new RequestError($res->errorMessage, null, $res);
+            throw new RequestException($res->errorMessage, null, $res);
         }
 
         return $res->data;
