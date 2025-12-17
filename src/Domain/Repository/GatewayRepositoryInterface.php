297dc1ec1da6132f77a646ba273776ec37945018
Martin Supiot <martin.supiot@getalma.eu>
2025-12-17 18:07:12 +0100

    feat: refactor interface

--- src/Domain/Repository/GatewayRepositoryInterface.php
+++ src/Domain/Repository/GatewayRepositoryInterface.php
@@ -3,5 +3,5 @@
 namespace Alma\API\Domain\Repository;
 
 interface GatewayRepositoryInterface {
-    public function findAllAlmaGateways(): array;
+    public function findOrderedAlmaGateways(): array;
 }
