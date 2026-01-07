4fb5e69a17c88550de510d4df5e7c46c5c6cc1c7
Benjamin Freoua <89775252+Benjamin-Freoua-Alma@users.noreply.github.com>
2026-01-07 16:35:19 +0100

    feat: add business event repository interface (#226)
    * feat: add business event repository interface
    
    * feat: add merchantproviderinterface
    
    * feat: review [MAR]

---
+++ src/Domain/Repository/BusinessEventsRepositoryInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Alma\API\Domain\Repository;
+
+interface BusinessEventsRepositoryInterface
+{
+    /**
+     * Check if a cart ID exist and if is not converted yet in the database.
+     *
+     * @param int $cartId
+     * @return bool
+     */
+    public function isCartIdValid(int $cartId): bool;
+}
