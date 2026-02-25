efb0cb6ac88d9250c609c5f70d63c5cf51fd713d
Benjamin Freoua <benjamin.freoua@getalma.eu>
2026-02-25 09:06:50 +0100

    feat: remove fix out of this feature

--- src/Client/Application/Endpoint/MerchantEndpoint.php
+++ src/Client/Application/Endpoint/MerchantEndpoint.php
@@ -111,7 +111,7 @@ class MerchantEndpoint extends AbstractEndpoint
             } catch (ParametersException $e) {
                 throw new MerchantEndpointException($e->getMessage(), $request, $response);
             }
-            $feePlanList->add($feePlan);
+            $feePlanList->Add($feePlan);
         }
 
         return $feePlanList;
